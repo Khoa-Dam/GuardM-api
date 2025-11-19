332adb1935c088a8d9af8225bb840998a50d8c9c
Khoa Dam <damngockhoa0703@gmail.com>
2025-11-20 01:34:37 +0700

    Add users

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Role } from '../auth/enums/role.enum';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({
+    type: 'enum',
+    enum: Role,
+    default: Role.User,
+  })
+  role: Role;
+}
+
