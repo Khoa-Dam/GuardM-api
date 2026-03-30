a1e6d93fee1a5ae4cd43dce6aca059b3ee7585d6
Kaito <damngockhoa0703@gmail.com>
2026-03-30 16:55:23 +0700

    Redeploy

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import * as bodyParser from 'body-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://www.guardm.space',
+    origin: ['https://guardm.space'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
   });
